17fde345c608463dc4d255f12f248e9b38fee468
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2023-11-23 20:20:12 +0530

    test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 export default function Navbar() {
   const navbaritems = [
-    'Quotes',
-    'Jokes',
-    'Currency Converter',
-    'Random Cat/Dog Images',
-    'Language Flash card',
-    'Book Shelf',
-    'Expense Tracker',
-    'Count Down Timer',
-    'Fitness Tracker',
-    'Budget Calculator',
+    {title:'Quotes'},
+    {title:'Jokes'},
+    {title:'Currency Converter'},
+    {title:'Random Cat/Dog Images'},
+    {title:'Language Flash card'},
+    {title:'Book Shelf'},
+    {title:'Expense Tracker'},
+    {title:'Count Down Timer'},
+    {title:'Fitness Tracker'},
+    {title:'Budget Calculator'}
   ];
   return (
     <div className="absolute top-0 left-0 w-full bg-pink flex">
       {navbaritems &&
         navbaritems.map((item) => {
-          return <div>{item}</div>;
+          return <div>{item.title}</div>;
         })}
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App';
 
 const root = createRoot(document.getElementById('app'));
 
 root.render(
   <StrictMode>
-    <App />
+    <Router><App /></Router>
   </StrictMode>
 );
