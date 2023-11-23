0b5422131ac046f9bf56542cb590968fddeb7e59
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2023-11-23 20:38:38 +0530

    button added

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -3,7 +3,11 @@ export default function Quotes() {
         'quote1',
         'quote2'
     ]
+    const generateRandom = ()=>{
+        console.log('hello world')
+    }
     return <div>
         <h1>Quotes</h1>
+        <button className="bg-blue-300 p-2 hover:bg-blue-400 rounded-lg" type="button" onClick={()=>generateRandom()}>Generate Quotes</button>
         </div>
 }
