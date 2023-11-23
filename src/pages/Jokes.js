11bdb2b06088e24aa33cb9e4316827c260f0aa1a
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2023-11-23 21:00:03 +0530

    button logic

--- src/pages/Jokes.js
+++ src/pages/Jokes.js
@@ -1,3 +1,34 @@
-export default function Jokes(){
-    return <div>Jokes</div>
+import React,{useState,useEffect} from 'react'
+export default function Jokes() {
+    const [jokes,setJokes] = useState([
+        'joke1',
+        'joke2',
+        'joke3'
+    ])
+    const [isNewJoke,setRandomNewJoke]= useState(false) 
+    const [newJoke,setNewJoke]= useState('hello world')
+    const generateRandom = ()=>{
+        const randomGen = Math.floor(Math.random() * 3);
+        setJokes(jokes[randomGen])
+    }
+    const loadJokes = ()=>{
+        fetch('https://v2.jokeapi.dev/joke/Any?format=json')
+        .then(res=>res.json())
+        .then(result=>setJokes(result.joke))
+        .catch(err=>console.log(err))
+
+    }
+    useEffect(()=>{
+        loadJokes();
+    },[isNewJoke])
+    return <div>
+        <h1>jokes</h1>
+        <button 
+        className="bg-blue-300 p-2 hover:bg-blue-400 rounded-lg" 
+        type="button" onClick={()=>{
+            // generateRandom();
+        setRandomNewJoke(!isNewJoke)}}>
+            Generate jokes</button>
+        <div>{newJoke}</div>
+        </div>
 }
--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,13 +1,19 @@
+import React,{useState,useEffect} from 'react'
 export default function Quotes() {
-    const quotes = [
+    const [quotes,setQuotes] = useState([
         'quote1',
-        'quote2'
-    ]
+        'quote2',
+        'quote3'
+    ])
+    const [newQuote,setNewQuote]= useState('hello world')
     const generateRandom = ()=>{
         console.log('hello world')
+        const randomGen = Math.floor(Math.random() * 3);
+        setNewQuote(quotes[randomGen])
     }
     return <div>
         <h1>Quotes</h1>
         <button className="bg-blue-300 p-2 hover:bg-blue-400 rounded-lg" type="button" onClick={()=>generateRandom()}>Generate Quotes</button>
+        <div>{newQuote}</div>
         </div>
 }
