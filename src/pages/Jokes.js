02450a0c11ef41068a8951a263210d8e7d7f4f57
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2023-11-23 21:02:42 +0530

    button logic

--- src/pages/Jokes.js
+++ src/pages/Jokes.js
@@ -7,19 +7,19 @@ export default function Jokes() {
     ])
     const [isNewJoke,setRandomNewJoke]= useState(false) 
     const [newJoke,setNewJoke]= useState('hello world')
-    const generateRandom = ()=>{
-        const randomGen = Math.floor(Math.random() * 3);
-        setJokes(jokes[randomGen])
-    }
+    // const generateRandom = ()=>{
+    //     const randomGen = Math.floor(Math.random() * 3);
+    //     setJokes(jokes[randomGen])
+    // }
     const loadJokes = ()=>{
         fetch('https://v2.jokeapi.dev/joke/Any?format=json')
         .then(res=>res.json())
-        .then(result=>setJokes(result.joke))
+        .then(result=>setJokes(result['joke']))
         .catch(err=>console.log(err))
 
     }
     useEffect(()=>{
-        loadJokes();
+        return loadJokes();
     },[isNewJoke])
     return <div>
         <h1>jokes</h1>
@@ -29,6 +29,6 @@ export default function Jokes() {
             // generateRandom();
         setRandomNewJoke(!isNewJoke)}}>
             Generate jokes</button>
-        <div>{newJoke}</div>
+        <div>{isNewJoke} {newJoke}</div>
         </div>
 }
